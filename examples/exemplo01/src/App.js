aca3ec066b5995fd9a888b63c88efcd777ba8f9a
Duda Wiltiner <eduardawiltiner@gmail.com>
2022-01-20 14:12:48 -0300

    Atualizações das atividades

---
+++ examples/exemplo01/src/App.js
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Nome" />
+        <textarea placeholder="Fale sobre vc...." />
+      </form>
+
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h2>Título</h2>
+            </header>
+            <p>False sobre vc</p>
+          </section>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
